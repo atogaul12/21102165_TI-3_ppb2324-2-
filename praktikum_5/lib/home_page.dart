c4a321e781905c3622d3e4bbd0be304cc19c4189
atogaul12 <muhammadatoulloh@gmail.com>
2024-03-22 08:22:16 +0700

    Praktikum 5 Navigation dan Routing

--- praktikum_5/lib/home_page.dart
+++ praktikum_5/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:praktikum_5/second_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,7 +12,14 @@ class HomePage extends StatelessWidget {
       ),
     body: Center(
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context)=> const SecondPage(),
+              ),
+            );  
+          },
           child: const Text('Menuju halaman kedua'),
         ),
       ),
