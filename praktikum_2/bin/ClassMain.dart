5ef69ce2d4b91c4e0489d982809687705c0c146a
atogaul12 <muhammadatoulloh@gmail.com>
2024-05-20 18:53:18 +0700

    Praktikum 9 Firebase Authentication

--- praktikum_2/bin/ClassMain.dart
+++ praktikum_2/bin/ClassMain.dart
@@ -1,7 +1,7 @@
 import 'ClassHewan.dart';
 
 void main() {
-  var kucing = hewan('Ketty', 2, 3.2);
+  var kucing = hewan('Kettty', 2, 3.2);
   kucing.makan();
   kucing.tidur();
   print(kucing.berat);
